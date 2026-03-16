b486ded7a2af7351ea307681694b7c1d965575e7
Olga Pavlova <olni.pavlova@gmail.com>
2026-03-15 20:00:27 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  // const arr = useState(1);
+  // console.log(arr);
+  const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: "Sophie" });
+  function handlePrevious() {
+    // setStep(step - 1);
+    // setStep(step > 1 ? step - 1 : step)
+    if (step > 1) setStep(step - 1);
+  }
+  function handleNext() {
+    // setStep(step + 1);
+    // setStep((step) => (step < 3 ? step + 1 : step));
+    if (step < 3) setStep(step + 1);
+    setTest({ name: "Michelle" });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="steps">
+      <div className="numbers">
+        {/* <div className={`${step >= 1 ? "active" : ""}`}>1</div> */}
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step === 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+        {test.name}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          // onClick={() => alert("Previous!")}
+          onClick={handlePrevious}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+          // onMouseEnter={() => alert("Test")}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
