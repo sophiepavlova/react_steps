63b00ad969d669cd7d85302c6829777c56f1c19c
sophiepavlova <olni.pavlova@gmail.com>
2026-03-31 09:08:49 -0700

    Refactor: turn a button into component (my take)

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,14 +1,15 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const messages = [
-  'Learn React ⚛️',
-  'Apply for jobs 💼',
-  'Invest your new income 🤑',
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
 ];
 
 export default function App() {
   return (
     <div>
+      <Steps />
       <Steps />
     </div>
   );
@@ -33,43 +34,39 @@ function Steps() {
   }
   return (
     <div>
-      <button className='close' onClick={() => setIsOpen((is) => !is)}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
       {isOpen && (
-        <div className='steps'>
-          <div className='numbers'>
+        <div className="steps">
+          <div className="numbers">
             {/* <div className={`${step >= 1 ? "active" : ""}`}>1</div> */}
-            <div className={step >= 1 ? 'active' : ''}>1</div>
-            <div className={step >= 2 ? 'active' : ''}>2</div>
-            <div className={step === 3 ? 'active' : ''}>3</div>
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step === 3 ? "active" : ""}>3</div>
           </div>
-          <p className='message'>
+          <p className="message">
             Step {step}: {messages[step - 1]}
             {/* {test.name} */}
           </p>
-          <div className='buttons'>
-            <Button
-              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              // onClick={() => alert("Previous!")}
               onClick={handlePrevious}
-              text='⬅️ Previous'
-            />
-            <Button
-              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handleNext}
-              text='Next ➡️'
-            />
+              // onMouseEnter={() => alert("Test")}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ style, onClick, text }) {
-  return (
-    <button style={style} onClick={onClick}>
-      {text}
-    </button>
   );
 }
